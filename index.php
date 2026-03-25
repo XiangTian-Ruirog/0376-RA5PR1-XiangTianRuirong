79db21c01d1017c49ee52fe4c71e0c127b6bec81
Ruirong Xiang <rxiang25@ilb.cat>
2026-03-25 12:53:32 +0100

    Funcio bucle  per la taula de multiplicacio i ifs per saber si son parells o senars creats

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 $num = 7;
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primer codi en PHP</title>
     <style>
         table {
@@ -32,25 +30,28 @@ $num = 7;
     </style>
 </head>
 <body>
-    <h2>Taula de multiplicar</h2>
-    <?php
-    if ($num <1 || $num > 12){
-        echo "<pclass='error'>Error: El número ha d'estar entre 1 i 12</p>";
-    } else {
-        echo "<table>";
-        for ($i = 1;$i <= 10; $i++) {
-            if ($i % 2 == 0) {
-                $classe = "parell";
-            } else {
-                $classe = "senar";
-            }
-            echo "<tr class='$classe'>";
-            echo "<td>$num x $i</td>";
-            echo "<td>" . ($num * $i) . "</td>";
-            echo "</tr>";
+<h2>Taula de multiplicar</h2>
+<?php
+if ($num < 1 || $num > 12) {
+    echo "<p class='error'>Error: El número ha d'estar entre 1 i 12.</p>";
+} else {
+    echo "<table>";
+    for ($i = 1; $i <= 10; $i++) {
+        if ($i % 2 == 0) {
+            $classe = "parell";
+        } else {
+            $classe = "senar";
         }
-        echo "</table>";
+
+        echo "<tr class='$classe'>";
+        echo "<td>$num x $i</td>";
+        echo "<td>" . ($num * $i) . "</td>";
+        echo "</tr>";
     }
-    ?>
+
+    echo "</table>";
+}
+?>
+
 </body>
 </html>
